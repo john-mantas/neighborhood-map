230533225add87b894ed9ae2266ffbacabe5feba
John Mantas <mantasjohn67@gmail.com>
2018-08-17 16:17:12 +0300

    feat: Add map container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/Header'
+import Map from './components/map/Map'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <main>
+          <Map />
+        </main>
       </div>
     );
   }
---
+++ src/components/map/Map.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Map.css'
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div id="map">
+      
+      </div>
+    );
+  }
+}
+
+export default Map
