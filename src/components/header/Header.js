b7db47c7b5fa78b9be4a0489f010eb5fd3b9d3a0
John Mantas <mantasjohn67@gmail.com>
2018-08-17 15:36:54 +0300

    feat: Add header on the page

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+      <button className="header__menu-btn"><i className="material-icons">menu</i></button>
+        <h1 className="header__title">ACROPOLIS TOUR</h1>
+      </header>
+    );
+  }
+}
+
+export default Header
