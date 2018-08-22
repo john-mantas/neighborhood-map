3cb98cea2182edc69de21a3ba16accaf25d22b41
John Mantas <mantasjohn67@gmail.com>
2018-08-22 22:51:22 +0300

    feat: Apply new style for the map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -12,7 +12,120 @@ class Map extends React.Component {
     google = window.google
     this.props.parentState.map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: 37.9713719, lng: 23.7264101 },
-      zoom: 17
+      zoom: 16,
+      styles: [
+        {
+          "featureType": "administrative.locality",
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "on"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.locality",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#000000"
+            },
+            {
+              "visibility": "on"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.locality",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "visibility": "on"
+            },
+            {
+              "color": "#ffffff"
+            },
+            {
+              "weight": "0.75"
+            }
+          ]
+        },
+        {
+          "featureType": "landscape.natural",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "visibility": "on"
+            },
+            {
+              "color": "#ded7c6"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "visibility": "on"
+            },
+            {
+              "color": "#ded7c6"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "lightness": 100
+            },
+            {
+              "visibility": "simplified"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "visibility": "on"
+            },
+            {
+              "lightness": 700
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+            {
+              "color": "#c3a866"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        }
+      ]
     });
     this.props.update()
     console.log('initMap updated the map')
